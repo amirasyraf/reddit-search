ffaf914bb450c91ee4f9b93039d13b42d632735b
Amir <amirasyraf@outlook.com>
2019-08-30 14:31:54 +0800

    defined Osky namespace

--- src/osky.php
+++ src/osky.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php'; 
 
 use Symfony\Component\Console\Application; 
-use Console\App;
+use Osky\App;
 
 $app = new Application('Reddit Search', 'v0.1.0');
 
