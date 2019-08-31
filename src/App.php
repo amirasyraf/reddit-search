4042820b60e07ed1a0d418eb962415bca0b7c553
Amir <amirasyraf@outlook.com>
2019-08-31 10:31:34 +0800

    entry point

---
+++ src/App.php
@@ -0,0 +1,23 @@
+<?php 
+namespace Osky;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Osky\Command;
+
+class App extends Command
+{
+    
+    public function configure()
+    {
+        $this -> setName('reddit:search')
+            -> setDescription('Search a given subreddit')
+            -> setHelp('Search a given subreddit');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this -> search($input, $output);
+    }
+}
